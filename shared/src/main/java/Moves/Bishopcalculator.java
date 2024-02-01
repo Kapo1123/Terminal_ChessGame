07fe17014d6e9335e48a9f799546b0679a3794a0
kapo <kpk23@byu.edu>
2024-01-31 23:08:09 -0700

    Found bugs on Rook and Bishop calculator, replaced the code with my other chess file.

--- shared/src/main/java/Moves/Bishopcalculator.java
+++ shared/src/main/java/Moves/Bishopcalculator.java
@@ -1,47 +1,38 @@
 package Moves;
+
 import chess.*;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class Bishopcalculator implements calculator {
+public class Bishopcalculator implements calculator{
   public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color){
-    ArrayList moves = new ArrayList<>();
+    ArrayList<ChessMove> moves = new ArrayList<>();
     moves.addAll(helper(board,myPosition,color,1,1));
     moves.addAll(helper(board,myPosition,color,1,-1));
     moves.addAll(helper(board,myPosition,color,-1,1));
     moves.addAll(helper(board,myPosition,color,-1,-1));
+
     return moves;
-
-
-
-
-
   }
-  public Collection<ChessMove> helper(ChessBoard board, ChessPosition myPosition,ChessGame.TeamColor color,int first, int second){
-    ArrayList moves = new ArrayList<ChessMove>();
-    int col = myPosition.getColumn();
-    int row = myPosition.getRow();
-    while (col <8 && row<8 && col+second>=1 && row+first>=1){
-      if (board.getPiece(new ChessPosition(row+first,col+second)) != null){
-        ChessPiece chess = board.getPiece(new ChessPosition(row+first,col+second));
-        if (chess.getTeamColor().equals(color)){
-            break;
+  public Collection<ChessMove> helper(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color,int first, int second){
+    ArrayList<ChessMove> moves = new ArrayList<>();
+    int row = myPosition.getRow()+first;
+    int col =myPosition.getColumn() +second;
+    while(row<=8&&col<=8&&row>=1&&col>=1){
+      ChessPiece piece = board.getPiece(new ChessPosition(row,col));
+      if(piece != null){
+        if (piece.getTeamColor() == color){
+          return moves;
         }
-        else {
-          moves.add( new ChessMove(myPosition,new ChessPosition(row+first,col+second), null));
-          break;
-        }
-
+        moves.add(new ChessMove(myPosition,new ChessPosition(row,col),null));
+        return moves;
       }
-      moves.add( new ChessMove(myPosition,new ChessPosition(row+first,col+second), null));
-      row += first;
-      col+= second;
-
+      moves.add(new ChessMove(myPosition,new ChessPosition(row,col),null));
+      row+=first;
+      col+=second;
     }
     return moves;
   }
+
 }
-
-
