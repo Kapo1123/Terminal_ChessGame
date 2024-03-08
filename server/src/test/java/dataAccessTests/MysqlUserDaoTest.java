65fbc33f4d9782376441ec1c937a6c35d7aada4f
kapo <kpk23@byu.edu>
2024-03-07 19:06:56 -0700

    Finished User Dao test cases

--- server/src/test/java/dataAccessTests/MysqlUserDaoTest.java
+++ server/src/test/java/dataAccessTests/MysqlUserDaoTest.java
@@ -1,7 +1,68 @@
 package dataAccessTests;
 
+import Requestclasses.Authtoken;
+import Requestclasses.Registerclass;
+import Requestclasses.Userclass;
+import dataAccess.DataAccessException;
+import dataAccess.MysqlAuthDao;
+import dataAccess.MysqlUserDao;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MysqlUserDaoTest {
+  MysqlUserDao user=new MysqlUserDao();
+  private String username = "kapo";
+  private String password = "1234";
+  private String email = "HEllothere@yahoo.com";
+  @BeforeEach
+  public void clear() throws DataAccessException {
+    user.deleteall();
+  }
+  @Test
+  @Order(1)
+  public void createUser_positive() throws DataAccessException {
+    Registerclass register = new Registerclass(username,password,email);
+    user.createUser(register);
+    Userclass info = new Userclass(username,password);
+    var ans  = user.checkCredential(info);
+    Assertions.assertTrue(ans,"The function should return True");
+  }
+  @Test
+  @Order(1)
+  public void createUser_negative() throws DataAccessException {
+    Registerclass register = new Registerclass(username,password,email);
+    user.createUser(register);
+    Assertions.assertThrows(DataAccessException.class, () -> {
+      user.createUser(register);
+    }, "id 0 should never be a game ID");
+  }
+  @Test
+  @Order(1)
+  public void checkCredential_positive() throws DataAccessException {
+    Registerclass register = new Registerclass(username,password,email);
+    user.createUser(register);
+    Userclass info = new Userclass(username,password);
+    var ans  = user.checkCredential(info);
+    Assertions.assertTrue(ans,"The function should return True");
+  }
+  @Test
+  @Order(1)
+  public void checkCredential_negative() throws DataAccessException {
+    Userclass info = new Userclass(username,password);
+    var ans  = user.checkCredential(info);
+    Assertions.assertFalse(ans,"The function should return True");
+  }
+  @Test
+  @Order(1)
+  public void user_clear() throws DataAccessException {
+   user.deleteall();
+  }
+
 
 }
