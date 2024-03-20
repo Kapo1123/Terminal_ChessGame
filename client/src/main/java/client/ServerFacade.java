fe228afa74fc846a8e9e39cdb5c512419e8df126
kapo1123 <kpk23@byu.edu>
2024-03-20 15:18:56 -0600

    Finished creating repl and main.

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,10 +1,81 @@
 package client;
 
+import Responseclass.Registerresponse;
+
+import java.util.Arrays;
+
 public class ServerFacade {
   private final String serverUrl;
 
   public ServerFacade(String url) {
     serverUrl = url;
   }
+  public Registerresponse  register(String[] params){
+    var path = "/user";
+    return this.makeRequest("POST", path, params, Registerresponse.class);
+  }
+  public Registerresponse  login(String[] params){
+    var path = "/session";
+    return this.makeRequest("POST", path, params, Registerresponse.class);
+  }
+
+  public Registerresponse  creategame(String[] params){
+    var path = "/game";
+    return this.makeRequest("POST", path, params, Registerresponse.class);
+  }
+  public Registerresponse  listGame(String[] params){
+    var path = "/game";
+    return this.makeRequest("GET", path, params, Registerresponse.class);
+  }
+  public Registerresponse  logout(){
+    var path = "/session";
+    return this.makeRequest("DELETE", path, Registerresponse.class);
+  }
+  public Registerresponse  joinGame(String[] params){
+    var path = "/game";
+    return this.makeRequest("PUT", path, params, Registerresponse.class);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
